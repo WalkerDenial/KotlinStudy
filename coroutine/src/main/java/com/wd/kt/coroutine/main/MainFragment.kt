3b9ab6729ade04e064fa85adfc6a5daa7640af75
Walker Denial <WalkerDenial@qq.com>
2020-11-06 23:52:59 +0800

    add databing

--- coroutine/src/main/java/com/wd/kt/coroutine/main/MainFragment.kt
+++ coroutine/src/main/java/com/wd/kt/coroutine/main/MainFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.wd.kt.coroutine.R
@@ -15,12 +17,15 @@ class MainFragment : Fragment() {
     }
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var binding: ViewDataBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.main_fragment, container, false)
+        binding.lifecycleOwner = this
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
--- coroutine/src/main/java/com/wd/kt/coroutine/main/MainViewModel.kt
+++ coroutine/src/main/java/com/wd/kt/coroutine/main/MainViewModel.kt
@@ -1,6 +1,10 @@
 package com.wd.kt.coroutine.main
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
+
+    val msg = MutableLiveData<String>().apply { value = "Test" }
+
 }
