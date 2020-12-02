bdcd7640cb0463197bcafb20a26f135292c98c97
Walker Denial <WalkerDenial@qq.com>
2020-12-02 23:55:43 +0800

    update code

---
+++ demo_annotation/src/main/java/com/wd/kt/annotation/IntDefTest.kt
@@ -0,0 +1,22 @@
+package com.wd.kt.annotation
+
+/**
+ *
+ * @Author Walker Denial
+ * @Email WalkerDenial@gmail.com
+ * @Time Created at 12/2/20 11:41 PM.
+ */
+
+/**
+ * 列举周几信息
+ * 常规写法：枚举
+ */
+enum class Weeks {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
