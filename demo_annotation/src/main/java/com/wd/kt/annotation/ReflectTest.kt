a1e6272607d9126323a00f53f1dc6d5cf3fedf6a
Walker Denial <WalkerDenial@qq.com>
2020-12-04 23:54:30 +0800

    edit code

---
+++ demo_annotation/src/main/java/com/wd/kt/annotation/ReflectTest.kt
@@ -0,0 +1,14 @@
+package com.wd.kt.annotation
+
+/**
+ *
+ * @Author Walker Denial
+ * @Email WalkerDenial@gmail.com
+ * @Time Created at 12/4/20 11:53 PM.
+ * @Desc 测试反射信息
+ */
+class ReflectTest {
+
+    // TODO: 12/4/20  
+    
+}
